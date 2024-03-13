3e05fac44509656f55ca0895519eb7f71907e051
Pawan <paliwalpawan505@gmail.com>
2024-03-13 19:54:54 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
+
 function App() {
-  return <h1 className="text-3xl text-red-500">App-js</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/dashBoard" element={<DashBoard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return <div>PRojects</div>;
+};
+
+export default Projects;
