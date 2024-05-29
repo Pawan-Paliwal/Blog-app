a9dcd609556b8ba04aba7b7ca47d37e09898d333
Pawan <paliwalpawan505@gmail.com>
2024-05-29 23:15:07 +0530

    user delete functionality updated

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -36,6 +36,7 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
 export const signout = async (req, res, next) => {
   try {
     res.clearCookie("accesss_token").status(200).json("User has been Signout");
