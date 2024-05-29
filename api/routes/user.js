0ce0f13772f821032cd9c89cf689e9721d6b10da
Pawan <paliwalpawan505@gmail.com>
2024-05-29 22:47:08 +0530

    adding getuser all api routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { test, updateUser, deleteUser, signout } from "../controllers/user.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  signout,
+  getUsers,
+} from "../controllers/user.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
@@ -7,5 +13,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.get("/test", test);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
