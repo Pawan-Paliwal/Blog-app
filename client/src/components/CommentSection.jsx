0140efff3b645aefb8fb882e434a3b3a77f67fe3
Pawan <paliwalpawan505@gmail.com>
2024-05-31 20:29:45 +0530

    Add like functionality to the comment section

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Textarea } from "flowbite-react";
 import Comment from "./Comment";
 
@@ -9,6 +9,7 @@ export default function CommentSection({ postId }) {
   const [comment, setComment] = useState("");
   const [commentError, setcommentError] = useState('');
   const [Comments, setComments] = useState([]);
+  const navigate = useNavigate();
 
   console.log(Comments)
 
@@ -55,6 +56,30 @@ export default function CommentSection({ postId }) {
     }
     getComments();
   }, [postId])
+
+  const handleLike = async (commentId) => {
+    try {
+      if(!currentUser) {
+        navigate('/sign-in')
+        return;
+      }
+      const res = await fetch(`/api/comment/likecomment/${commentId}`, {
+        method: "PUT",
+      });
+      if(res.ok) {
+        const data = await res.json();
+        setComment(Comments.map((comment) => {
+          comment._id === commentId ? {
+            ...comment, likes: data.likes,
+            numberOfLikes: data.likes.length
+          } : comment
+        }))
+      }
+    } catch(error) {
+      console.log(error.message)
+    }
+  }
+
 
 
   return (
@@ -124,6 +149,7 @@ export default function CommentSection({ postId }) {
               <Comment
                 key={comment._id}
                 comment={comment}
+                onLike={handleLike}
               />
             ))}
           </>
