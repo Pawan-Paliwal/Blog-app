c12f4a7364b7c8b84413cdc4b6058098695f627f
Pawan <paliwalpawan505@gmail.com>
2024-05-31 12:04:07 +0530

    Add comment section to the page Post

--- api/index.js
+++ api/index.js
@@ -8,12 +8,14 @@ const app = express();
 import userRoutes from "../api/routes/user.js";
 import authroutes from "../api/routes/auth.js";
 import postroutes from "../api/routes/post.js";
+import Commentroutes from "../api/routes/comment.js";
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authroutes);
 app.use("/api/post", postroutes);
+app.use("/api/comment", Commentroutes);
 
 mongoose
   .connect(
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,12 @@ import CreatePost from "./pages/CreatePost";
 import OnlyAdminPrivate from "./components/OnlyAdminPrivate";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
